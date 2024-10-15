a83e3f600e2c1434de1d3d7f9bdda53b611381e8
Kiárá Kamaridisz <kiara.kamaridisz@gramont.ch>
2024-10-15 09:33:14 +0000

    new ui

--- capllmpocui/webapp/controller/App.controller.js
+++ capllmpocui/webapp/controller/App.controller.js
@@ -5,7 +5,7 @@ sap.ui.define(
     function(BaseController) {
       "use strict";
   
-      return BaseController.extend("capllmui.controller.App", {
+      return BaseController.extend("capllmpocui.controller.App", {
         onInit: function() {
         }
       });
